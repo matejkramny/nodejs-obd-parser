57a4c89c34162b9ba5045cebe0729c7af427e6fc
Matej Kramny <matejkramny@gmail.com>
2014-12-04 20:35:03 +0000

    rm test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-var Table = require('cli-table');
-var table = new Table({ head: ["", "Top Header 1", "Top Header 2"] });
-
-table.push(
-    { 'Left Header 1': ['Value Row 1 Col 1', 'Value Row 1 Col 2'] }
-  , { 'Left Header 2': ['Value Row 2 Col 1', 'Value Row 2 Col 2'] }
-);
-
-console.log(table.toString());
